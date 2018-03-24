2a9cdb7204088b4745a85ff79526712af7e063ec
uwsgi <devopsscion@devopsscion.com>
2018-03-24 22:57:51 +0000

    sudo update nproc version update

--- python/python.sh
+++ python/python.sh
@@ -16,34 +16,35 @@ sudo apt-fast -qq -y install wget sudo vim curl build-essential
 
 sudo apt-fast -qq -y install libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
 pushd /tmp
-sudo wget --quiet https://www.kernel.org/pub/software/scm/git/git-${GIT_VERSION}.tar.gz
-sudo tar -xvf git-${GIT_VERSION}.tar.gz
+wget --quiet --continue https://www.kernel.org/pub/software/scm/git/git-${GIT_VERSION}.tar.gz
+tar -xvf git-${GIT_VERSION}.tar.gz
 pushd git-${GIT_VERSION}
-sudo make --silent prefix=/usr/local all && sudo make --silent prefix=/usr/local install
+make --silent prefix=/usr/local all && sudo make --silent prefix=/usr/local install
 popd
 popd
 
 sudo apt-fast -qq -y install sqlite3 libsqlite3-dev libssl-dev zlib1g-dev libxml2-dev libxslt-dev libbz2-dev gfortran libopenblas-dev liblapack-dev libatlas-dev subversion
 
 pushd /tmp
-sudo wget --quiet https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz -O /tmp/Python-${PYTHON_VERSION}.tgz
-sudo tar -xvf Python-${PYTHON_VERSION}.tgz
+wget --quiet --continue https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz -O /tmp/Python-${PYTHON_VERSION}.tgz
+tar -xvf Python-${PYTHON_VERSION}.tgz
 pushd Python-${PYTHON_VERSION}
-sudo ./configure CFLAGSFORSHARED="-fPIC" CCSHARED="-fPIC" --quiet CCSHARED="-fPIC" --prefix=/usr/local/opt/python --exec-prefix=/usr/local/opt/python CCSHARED="-fPIC" \
-            && make clean && make --silent -j3 && sudo make --silent install
+./configure CFLAGSFORSHARED="-fPIC" CCSHARED="-fPIC" --quiet CCSHARED="-fPIC" --prefix=/usr/local/opt/python --exec-prefix=/usr/local/opt/python CCSHARED="-fPIC" \
+            && make clean && make --silent -j$(nproc) && sudo make --silent install
 popd
 
-sudo ln -s /usr/local/opt/python/bin/python /usr/local/bin/python
+sudo ln -sf /usr/local/opt/python/bin/python3 /usr/local/bin/python3
 
-which python && python --version
+which python3 && python3 --version
 
 pushd /tmp
-sudo wget --quiet https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py
-#sudo ln -s /usr/local/opt/python/bin/pip /usr/local/bin/pip
+wget --quiet --continue https://bootstrap.pypa.io/get-pip.py && sudo python3 get-pip.py
+popd
+sudo ln -sf /usr/local/opt/python/bin/pip3 /usr/local/bin/pip3
 
-sudo pip install -U setuptools-git==1.2 wheel==0.29.0 pipenv
-sudo pip install --upgrade pip
-#RUN pip install -U distribute==0.7.3  setuptools==8.3
+sudo pip3 install -U setuptools-git==1.2 wheel==0.29.0 pipenv
+sudo pip3 install --upgrade pip
+#RUN pip3 install -U distribute==0.7.3  setuptools==8.3
 
 sudo mkdir -p /wheelhouse
 
--- web/python-nginx/nginx.sh
+++ web/python-nginx/nginx.sh
@@ -2,20 +2,23 @@
 
 . ./nginxenv.sh
 
-sudo useradd uwsgi
+id -u uwsgi &>/dev/null || sudo useradd uwsgi
 
 sudo apt-fast install -y supervisor rsyslog-gnutls
 
 pushd /tmp
-wget --quiet ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.bz2 && \
-    tar -xvf pcre-8.38.tar.bz2
-pushd pcre-8.38 
-./configure && make --silent -j 3 && sudo make --silent install
+
+PCRE_VERSION=8.42
+
+wget --quiet ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.bz2 && \
+    tar -xvf pcre-${PCRE_VERSION}.tar.bz2
+pushd pcre-${PCRE_VERSION}
+./configure && make --silent -j$(nproc) && sudo make --silent install
 popd
 
 wget --quiet http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz && tar -xvf nginx-$NGINX_VERSION.tar.gz
 pushd nginx-$NGINX_VERSION 
-./configure --with-http_stub_status_module --with-http_ssl_module && sudo make --silent -j 3 && sudo make --silent install
+./configure --with-http_stub_status_module --with-http_ssl_module && make --silent -j$(nproc) && sudo make --silent install
 popd
 
 sudo apt-fast install -y libgeos-dev
@@ -23,13 +26,13 @@ sudo apt-fast install -y libgeos-dev
 #http://security.stackexchange.com/questions/95178/diffie-hellman-parameters-still-calculating-after-24-hours
 cd /etc/ssl/certs && sudo openssl dhparam -dsaparam -out dhparam.pem 2048
 
-sudo pip install uwsgi==$UWSGI_VERSION && \
+sudo pip3 install uwsgi==$UWSGI_VERSION && \
     sudo mkdir -p /var/log/uwsgi && \
     sudo mkdir -p /var/run/uwsgi && \
     sudo chown -R uwsgi /var/run/uwsgi && \
     sudo chown -R uwsgi /var/log/uwsgi
 
-popd
+popd #/tmp
 
 sudo cp conf/nginx.conf /usr/local/nginx/conf/nginx.conf
 sudo cp conf/supervisor-nginx.conf /etc/supervisor/conf.d/nginx.conf
