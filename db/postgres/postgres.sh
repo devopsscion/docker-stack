efc4a49d98ae72fa962756d86d275563ac0d31d4
uwsgi <devopsscion@devopsscion.com>
2018-03-25 01:51:58 +0000

    locale

--- db/postgres/postgres.sh
+++ db/postgres/postgres.sh
@@ -15,6 +15,10 @@ sudo add-apt-repository -y ppa:saiarcot895/myppa && \
     sudo apt-get -qq update && \
     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install apt-fast
 
+sudo apt-fast -qq -y install locales
+sudo locale-gen "en_US.UTF-8"
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+
 sudo apt-fast -qq -y install git python-dev python-pip wget sudo vim supervisor
 
 # Add the PostgreSQL PGP key to verify their Debian packages.
@@ -30,8 +34,8 @@ sudo apt-fast -qq update
 sudo apt-fast -y -qq install postgresql-${POSTGRES_VERSION} postgresql-client-${POSTGRES_VERSION} postgresql-contrib-${POSTGRES_VERSION} postgresql-server-dev-${POSTGRES_VERSION} libpq5 libpq-dev postgresql-${POSTGRES_VERSION}-postgis-2.2
 
 #Fix locale warnings when starting postgres
-sudo locale-gen en_US.UTF-8 && \
-    sudo dpkg-reconfigure locales
+#sudo locale-gen en_US.UTF-8 && \
+#    sudo dpkg-reconfigure locales
 
 
 
